b70f4b7a7890ce60b9f0b9f1165217f2a9c3484f
Punit Lad <punitlad@gmail.com>
2021-09-07 22:30:49 -0400

    reset pair stair when one member has reach a max number of rotations

--- models/pair_stair.go
+++ models/pair_stair.go
@@ -7,6 +7,7 @@ import (
 
 type PairStair struct {
 	Members []Member
+	maxRotations int
 }
 
 func NewPairStair(members []Member) (PairStair, error) {
@@ -14,11 +15,15 @@ func NewPairStair(members []Member) (PairStair, error) {
 		return PairStair{}, errors.New("Not enough members to create pair stair! Make sure to add more than 3 members.")
 	}
 
-	return PairStair{Members: members}, nil
+	return PairStair{members, len(members) - 1}, nil
 }
 
 func (stair *PairStair) Rotate() {
 	var assigned []string
+
+	if shouldReset(stair) {
+		reset(stair)
+	}
 
 	for i, member := range stair.Members {
 		if !util.Contains(assigned, member.Name) {
@@ -36,4 +41,22 @@ func (stair *PairStair) Rotate() {
 			}
 		}
 	}
+}
+
+func reset(stair *PairStair) {
+	for i, member := range stair.Members {
+		member.Reset()
+		stair.Members[i] = member
+	}
+}
+
+func shouldReset(stair *PairStair) bool {
+	shouldReset := false
+	for _, member := range stair.Members {
+		if len(member.PairedWith) == stair.maxRotations {
+			shouldReset = true
+		}
+	}
+
+	return shouldReset
 }
--- models/pair_stair_test.go
+++ models/pair_stair_test.go
@@ -12,10 +12,11 @@ func Test_RotateMembers(t *testing.T) {
 
 	stair.Rotate()
 
-	assert.Contains(t, stair.Members, Member{"Bender", []string{"Zoidberg"}, "Zoidberg"})
-	assert.Contains(t, stair.Members, Member{"Zoidberg", []string{"Bender"}, "Bender"})
-	assert.Contains(t, stair.Members, Member{"Fry", []string{"Leela"}, "Leela"})
-	assert.Contains(t, stair.Members, Member{"Leela", []string{"Fry"}, "Fry"})
+	for _, member := range stair.Members {
+		assert.Len(t, member.PairedWith, 1)
+		assert.NotEmpty(t, member.CurrentPair)
+		assert.NotEqual(t, member.Name, member.CurrentPair)
+	}
 }
 
 func Test_CannotCreateStairLessThanFourMembers(t *testing.T) {
@@ -25,4 +26,20 @@ func Test_CannotCreateStairLessThanFourMembers(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.EqualError(t, err, "Not enough members to create pair stair! Make sure to add more than 3 members.")
+}
+
+func Test_ResetStairsWhenRotationMaxIsReached(t *testing.T) {
+	members := []Member{
+		{"Bender", []string{"Zoidberg", "Leela", "Fry"}, "Zoidberg"},
+		{"Zoidberg", []string{"Bender", "Leela", "Fry"}, "Bender"},
+		{"Leela", []string{"Zoidberg", "Bender", "Fry"}, "Fry"},
+		{"Fry", []string{"Zoidberg", "Leela", "Bender"}, "Leela"},
+	}
+	stair, _ := NewPairStair(members)
+
+	stair.Rotate()
+
+	for _, member := range stair.Members {
+		assert.Len(t, member.PairedWith, 1)
+	}
 }
